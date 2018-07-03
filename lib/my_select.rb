1e15148dcb024e8ce3733c8b3854c64eb85e12b1
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-03 02:07:20 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,12 @@ def my_select(collection)
   i = 0
   new_collection = []
   while i < collection.length
+  
     block_given?
-    new_collection << yield(collection[i])
+    if yield collection[i]
+      new_collection << collection[i]
+    end
+    
     i += 1
   end
   new_collection
